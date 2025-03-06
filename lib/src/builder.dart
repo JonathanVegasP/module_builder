4041f276aae7f31e5275e2d39f2ee55be954255f
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2025-03-06 11:17:24 -0300

    added builder

---
+++ lib/src/builder.dart
@@ -0,0 +1,6 @@
+import 'package:build/build.dart';
+import 'package:module_builder/src/module_builder.dart';
+
+Builder moduleBuilder(BuilderOptions options) {
+  return ModuleBuilder();
+}
---
+++ lib/src/module_builder.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+
+import 'package:build/build.dart';
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+import 'package:glob/glob.dart';
+import 'package:module/module.dart';
+import 'package:path/path.dart';
+import 'package:source_gen/source_gen.dart';
+
+final class ModuleBuilder extends Builder {
+  final _allFiles = Glob('**/**.dart');
+
+  static const superModuleChecker = TypeChecker.fromRuntime(Module);
+
+  @override
+  Map<String, List<String>> get buildExtensions => {
+    r'$lib$': ['config/modules/modules.dart'],
+  };
+
+  @override
+  FutureOr<void> build(BuildStep buildStep) async {
+    final imports = <Directive>[];
+    final buffer = StringBuffer();
+
+    var isConst = 'const ';
+
+    await for (final input in buildStep.findAssets(_allFiles)) {
+      if (!await buildStep.resolver.isLibrary(input)) continue;
+
+      final reader = LibraryReader(await buildStep.resolver.libraryFor(input));
+
+      final modules = reader.classes
+          .where(
+            (el) =>
+                superModuleChecker
+                    .isAssignableFrom(el),
+          )
+          .map((el) {
+            if (el.unnamedConstructor == null) {
+              throw UnsupportedError(
+                '${el.name}: Cannot use Module without an unnamed constructor',
+              );
+            }
+
+            return el.name;
+          })
+          .join('(),');
+
+      if (modules.isEmpty) continue;
+
+      if (isConst.isNotEmpty) {
+        for (final c in reader.classes) {
+          if (c.unnamedConstructor!.isConst != true) {
+            isConst = '';
+          }
+        }
+      }
+
+      imports.add(
+        Directive(
+          (b) =>
+              b
+                ..type = DirectiveType.import
+                ..url =
+                    'package:${input.package}/${input.pathSegments.skip(1).join('/')}',
+        ),
+      );
+
+      buffer.write('$modules(),');
+    }
+
+    final library = Library(
+      (b) =>
+          b
+            ..directives.addAll(imports)
+            ..body.add(
+              Class(
+                (b) =>
+                    b
+                      ..name = "Modules"
+                      ..modifier = ClassModifier.final$
+                      ..constructors.add(
+                        Constructor(
+                          (b) =>
+                              b
+                                ..name = "_"
+                                ..constant = true,
+                        ),
+                      )
+                      ..methods.add(
+                        Method(
+                          (b) =>
+                              b
+                                ..name = 'init'
+                                ..static = true
+                                ..lambda = true
+                                ..returns = refer('Future<void>')
+                                ..modifier = MethodModifier.async
+                                ..body = Code(
+                                  'await Future.wait($isConst[$buffer].map((el) => el.init()))',
+                                ),
+                        ),
+                      ),
+              ),
+            ),
+    );
+
+    final output = AssetId(
+      buildStep.inputId.package,
+      join('lib', 'config', 'modules', 'modules.dart'),
+    );
+
+    return buildStep.writeAsString(
+      output,
+      DartFormatter(
+        languageVersion: DartFormatter.latestLanguageVersion,
+      ).format('${library.accept(DartEmitter.scoped())}'),
+    );
+  }
+}
